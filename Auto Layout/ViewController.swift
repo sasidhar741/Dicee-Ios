5c6d8f707654df02ad156c096232f890c34c32a3
sasidhar <sasidhar741@gmail.com>
2021-07-07 11:25:44 +0530

    Added assets

--- Auto Layout/ViewController.swift
+++ Auto Layout/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftDice: UIImageView!
+    @IBOutlet weak var rightDice: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceImagesArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        leftDice.image = diceImagesArray.randomElement()
+        rightDice.image = diceImagesArray.randomElement()
+    
+    }
+    
 }
 
